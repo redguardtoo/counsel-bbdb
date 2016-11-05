297d0c7e6e1eaafcd5e188724fea8e8f26b95555
Chen Bin <chenbin.sh@gmail.com>
2016-11-05 20:16:35 +1100

    first import

---
+++ pkg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+name=counsel-bbdb
+version=0.0.1
+pkg=$name-$version
+mkdir $pkg
+cp *.el $pkg
+cat << EOF > $pkg/$name-pkg.el
+(define-package "$name" "$version" "Quick search&input email from BBDB based on ivy")
+EOF
+if [[ `uname -s` == *Darwin* ]]; then
+   COPYFILE_DISABLE="" tar cvf $pkg.tar $pkg/
+else
+   tar cvf $pkg.tar $pkg/
+fi
+rm -rf $pkg/
