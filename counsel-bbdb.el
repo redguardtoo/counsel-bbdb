1ec50c8ae656a882d4239be4dc403d30955ec357
Chen Bin <chenbin.sh@gmail.com>
2017-10-12 23:59:30 +1100

    backup

--- counsel-bbdb.el
+++ counsel-bbdb.el
@@ -1,5 +1,3 @@
-;;; counsel-bbdb.el --- Quick search&input email from BBDB based on ivy
-
 ;; Copyright (C) 2016 Chen Bin
 ;;
 ;; Version: 0.0.1
@@ -31,9 +29,14 @@
 
 ;; Use `ivy-mode' to input email address from BBDB efficiently.
 ;; Smart to know some ethic groups display family name before given name
+;; Since It's not using any API from `bbdb', it's always fast and stable.
 ;;
 ;; `M-x counsel-bbdb-complete-mail' to input email address.
 ;; `M-x counsel-bbdb-reload' to reload contacts from BBDB database.
+;;
+;; Since counsel-bbdb is based ivy-mode. All ivy key bindings are supported.
+;; For example, after `C-u M-x counsel-bbdb-complete-mail', you can press
+;; `C-M-n' to input multiple mail address.
 ;;
 ;; You can also customize `counsel-bbdb-customized-insert' to insert
 ;; email in your own way:
@@ -66,6 +69,9 @@ If it's nil, the default insertion is executed.")
 (defvar counsel-bbdb-contacts nil
   "The contacts list read from `bbdb-file'.")
 
+(defvar counsel-bbdb-mail-alias-list nil
+  "The mail alias list.")
+
 (defun counsel-bbdb-family-name (r)
   "Get family name from R."
   (aref r 1))
@@ -81,6 +87,11 @@ If it's nil, the default insertion is executed.")
 (defun counsel-bbdb-emails (r)
   "Get emails from R."
   (aref r 7))
+
+(defun counsel-bbdb-mail-alias (r)
+  "Get emails from R."
+  (let* ((item (aref r 8)))
+    (if item (cdr (assoc 'mail-alias item)))))
 
 ;;;###autoload
 (defun counsel-bbdb-insert-string (str)
@@ -112,25 +123,39 @@ If it's nil, the default insertion is executed.")
     ;;   - given-name family-name:mail
     ;;   - :mail
     (setq counsel-bbdb-contacts nil)
+    (setq counsel-bbdb-mail-alias-list nil)
     (dolist (r raw-records)
       (let* ((full-name (counsel-bbdb-full-name r))
              (family-name (counsel-bbdb-family-name r))
              (given-name (counsel-bbdb-given-name r))
              (mails (counsel-bbdb-emails r))
+             (mail-alias (counsel-bbdb-mail-alias r))
              (prefix full-name))
-        (if (= (length prefix) 0)
-            (setq prefix (concat family-name
-                                 " "
-                                 given-name)))
-        (if (= (length prefix) 1)
-            (setq prefix ""))
+
+        (message "mail-alias=%s" mail-alias)
+        (when mail-alias
+          (let* ((strs (split-string mail-alias ", ")))
+            (dolist (s strs)
+              (add-to-list 'counsel-bbdb-mail-alias-list s))))
+
+        (when (= (length prefix) 0)
+          (setq prefix (concat family-name
+                               " "
+                               given-name)))
+        (if (= (length prefix) 1) (setq prefix ""))
 
         (dolist (m mails)
+          (message "mail-alist======%s" mail-alias)
           (add-to-list 'counsel-bbdb-contacts
                        (cons (concat prefix
                                      ":"
-                                     m)
-                             (list family-name given-name full-name m))))))))
+                                     m
+                                     (if mail-alias (format " => %s" mail-alias)))
+                             (list family-name
+                                   given-name
+                                   full-name
+                                   m
+                                   mail-alias))))))))
 
 ;;;###autoload
 (defun counsel-bbdb-complete-mail (&optional append-comma)
@@ -139,7 +164,7 @@ Extra argument APPEND-COMMA will append comma after email."
   (interactive "P")
   (unless counsel-bbdb-contacts
     (counsel-bbdb-reload))
-  (ivy-read "contacts: "
+  (ivy-read "Contacts: "
             counsel-bbdb-contacts
             :action `(lambda (r)
                        (let* (rlt
@@ -165,6 +190,16 @@ Extra argument APPEND-COMMA will append comma after email."
                              (setq rlt mail)))
                            (if append-comma (setq rlt (concat rlt ", "))))
                          (counsel-bbdb-insert-string rlt)))))
+
+(defun counsel-bbdb-expand-mail-alias ()
+  "Insert multiple mail address in alias/group."
+  (interactive)
+  (unless counsel-bbdb-contacts
+    (counsel-bbdb-reload))
+  (message "counsel-bbdb-mail-alias-list=%s" counsel-bbdb-mail-alias-list)
+  ;; we just need filter the cousnel-bbdb-contacts by selected alias
+  (ivy-read "Alias: "
+            counsel-bbdb-mail-alias-list))
 
 (provide 'counsel-bbdb)
 ;;; counsel-bbdb.el ends here
